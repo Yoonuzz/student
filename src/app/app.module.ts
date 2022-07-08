1eee8696aea7f0ee3cce8d5984cccdc4caf343aa
Yoonuzz <2001yoonus@gmail.com>
2022-07-08 10:07:25 +0530

    new student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewStudentComponent } from './new-student/new-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewStudentComponent
   ],
   imports: [
     BrowserModule,
